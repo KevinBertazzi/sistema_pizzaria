61ca3d8ecfbde712e0bb080e4fcc0a24c8b160b9
KevinBertazzi <huuj4875@gmail.com>
2026-02-05 17:26:21 -0300

    php

---
+++ backend/cadatrar.php
@@ -0,0 +1,47 @@
+<?php
+
+define('SERVIDOR','localhost');
+define('USUARIO','root');
+define('SENHA','');
+define('BANCO','db_pizzaria');
+//conexão ao banco
+
+
+try{
+    $conexao = new PDO("mysql:host=".SERVIDOR.";dbname=".BANCO.";charset=utf8",USUARIO,SENHA);
+
+    $conexao->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+//    echo "conectado com sucesso!";
+//    capitura os dados capitados pelo form
+
+$sabor = $_POST['sabor'];
+$ingrediente = $_POST['ingrediente'];
+$tamanho = $_POST['tamanho'];
+$valor = $_POST['valor'];
+
+$sql = "INSERT INTO tb_pizza (sabor,ingredinte,tamanho,valor)values('$sabor','$ingrediente','$tamanho','$valor')";
+
+//executp p query acima
+$comando = $conexao->prepare($sql);
+
+//executa o comando
+$comando->execute();
+
+//exibe a msg de sucesso
+echo"cadatro realizado com sucesso";
+
+
+
+}catch(PDOEexception $erro){
+   echo "erro ao conectao ao banco de dados .$erro";
+}
+
+
+
+
+
+
+
+
+?>
---
+++ backend/funcionario.php
@@ -0,0 +1,40 @@
+<php
+
+define('SERVIDOR','localhost');
+define('USUARIO','root');
+define('SENHA','');
+define('BANCO','db_pizzaria');
+//conexão ao banco
+
+
+try{
+    $conexao = new PDO("mysql:host=".SERVIDOR.";dbname=".BANCO.";charset=utf8",USUARIO,SENHA);
+
+    $conexao->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+//    echo "conectado com sucesso!";
+<!-- //    capitura os dados capitados pelo form -->
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$telefone = $_POST['telefone'];
+
+
+$sql = "INSERT INTO tb_funcionario (nome,email,telefone)values('$nome','$email','$telefone')";
+
+<!-- //executp p query acima -->
+$comando = $conexao->prepare($sql);
+
+<!-- //executa o comando -->
+$comando->execute();
+
+<!-- //exibe a msg de sucesso -->
+echo" Funcionario cadastrado com sucesso";
+
+
+
+}catch(PDOEexception $erro){
+   echo "erro ao conectao ao banco de dados .$erro";
+}
+
+?>
